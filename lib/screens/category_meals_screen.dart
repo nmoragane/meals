b214d2f96b9d60252272d980c758488e26086101
nmoragane <nmoragane@gmail.com>
2021-07-19 00:10:35 +0530

    meals model

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -18,10 +18,8 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes For the Category',
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {},
       ),
     );
   }
